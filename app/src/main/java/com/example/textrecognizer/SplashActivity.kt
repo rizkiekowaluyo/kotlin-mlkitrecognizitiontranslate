f40e2f94035c954920036a4b23a132393e5da848
rizkiekowaluyo <mochrizkiekow@gmail.com>
2019-10-07 21:33:32 +0700

    splash

---
+++ app/src/main/java/com/example/textrecognizer/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.textrecognizer
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        aviIndicator.setIndicator("BallPulseIndicator")
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2500)
+    }
+}
