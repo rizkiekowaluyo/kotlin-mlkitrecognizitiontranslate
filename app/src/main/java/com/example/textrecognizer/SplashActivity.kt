ecebb4e6e46884f9780ae012ac952d8d2d30a4c0
rizkiekowaluyo <mochrizkiekow@gmail.com>
2019-10-08 22:45:58 +0700

    login with logic

--- app/src/main/java/com/example/textrecognizer/SplashActivity.kt
+++ app/src/main/java/com/example/textrecognizer/SplashActivity.kt
@@ -19,7 +19,7 @@ class SplashActivity: AppCompatActivity() {
 
         aviIndicator.setIndicator("BallPulseIndicator")
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         },2500)
